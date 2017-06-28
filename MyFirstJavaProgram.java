c2d3105c7bfd691099904d29946c71101bf95450
adriananunez <adriana.nunez@jointheleague.org>
2017-06-28 13:58:03 -0700

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,28 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START 
+		Robot r2d2=new Robot();
+		r2d2.setPenColor(Color.MAGENTA);
+		r2d2.sparkle();
+		r2d2.penDown();
+		r2d2.move(100);
+		r2d2.turn(90);
+		r2d2.move(90);
+		r2d2.turn(90);
+		r2d2.move(90);
+		r2d2.turn(90);
+		r2d2.move(90);
+		r2d2.turn(90);
+		r2d2.
+		
+		
+		
+		
+		
+	}}
